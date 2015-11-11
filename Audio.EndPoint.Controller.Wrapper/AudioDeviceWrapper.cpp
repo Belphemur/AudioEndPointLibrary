ea0a3432df5af67347f13e9ee9edb21fdfd8d6c8
Antoine Aflalo <antoine@aaflalo.me>
2015-11-10 20:59:08 -0500

    Make the device check for the presence of the NotificationClient In case it's not present, raise the event of change of default

--- Audio.EndPoint.Controller.Wrapper/AudioDeviceWrapper.cpp
+++ Audio.EndPoint.Controller.Wrapper/AudioDeviceWrapper.cpp
@@ -1,57 +1,63 @@
 #include "stdafx.h"
 #include "AudioDeviceWrapper.h"
+#include "Audio.EndPoint.Controller.Wrapper.h"
 #include "DefSoundException.h"
 
 using namespace System;
 
-
-bool AudioEndPointControllerWrapper::AudioDeviceWrapper::Equals(Object ^ o)
-{
-    if (o == (Object^)nullptr)
-        return false;
-
-    if(o->GetType() != this->GetType())
+namespace AudioEndPointControllerWrapper {
+    bool AudioDeviceWrapper::Equals(Object ^ o)
     {
-        return false;
+        if (o == (Object^)nullptr)
+            return false;
+
+        if (o->GetType() != this->GetType())
+        {
+            return false;
+        }
+
+        AudioDeviceWrapper^ that = static_cast<AudioDeviceWrapper^>(o);
+        return that->Id == Id;
     }
 
-    AudioDeviceWrapper^ that = static_cast<AudioDeviceWrapper^>(o);
-    return that->Id == Id;
-}
+    int AudioDeviceWrapper::GetHashCode()
+    {
+        return this->Id->GetHashCode();
+    }
 
-int AudioEndPointControllerWrapper::AudioDeviceWrapper::GetHashCode()
-{
-    return this->Id->GetHashCode();
-}
+    void AudioDeviceWrapper::SetAsDefault(Role role)
+    {
+        try
+        {
+            _audioDevice->SetDefault(static_cast<::ERole>(role));
+            if (!AudioController::IsNotificationAvailable())
+            {
+                AudioController::RaiseDefault(gcnew DeviceDefaultChangedEvent(this, role));
+            }
+        }
+        catch (DefSound::CNotActiveError error)
+        {
+            throw gcnew DefSoundNotActiveException(error);
+        }
+        catch (DefSound::CError error)
+        {
+            throw gcnew DefSoundException(error);
+        }
+    }
 
-void AudioEndPointControllerWrapper::AudioDeviceWrapper::SetAsDefault(Role role)
-{
-	try 
-	{
-		_audioDevice->SetDefault(static_cast<::ERole>(role));
-	}
-	catch (DefSound::CNotActiveError error)
-	{
-		throw gcnew DefSoundNotActiveException(error);
-	}
-	catch(DefSound::CError error)
-	{
-		throw gcnew DefSoundException(error);
-	}
-}
-
-bool AudioEndPointControllerWrapper::AudioDeviceWrapper::IsDefault(Role role)
-{
-	try
-	{
-		return _audioDevice->IsDefault(static_cast<::ERole>(role));
-	}
-	catch (DefSound::CNotActiveError error)
-	{
-		throw gcnew DefSoundNotActiveException(error);
-	}
-	catch (DefSound::CError error)
-	{
-		throw gcnew DefSoundException(error);
-	}
+    bool AudioDeviceWrapper::IsDefault(Role role)
+    {
+        try
+        {
+            return _audioDevice->IsDefault(static_cast<::ERole>(role));
+        }
+        catch (DefSound::CNotActiveError error)
+        {
+            throw gcnew DefSoundNotActiveException(error);
+        }
+        catch (DefSound::CError error)
+        {
+            throw gcnew DefSoundException(error);
+        }
+    }
 }
